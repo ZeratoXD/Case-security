fa8b43e48ef2c9510f52883ff644b7b520d03683
ZeratoXD <ivan_cassio_dias@hotmail.com>
2024-04-14 17:27:46 -0300

    implementacao-MVC

---
+++ src/main/java/com/generation/autenticacao/AutenticacaologinApplication.java
@@ -0,0 +1,13 @@
+package com.generation.autenticacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutenticacaologinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutenticacaologinApplication.class, args);
+	}
+
+}
